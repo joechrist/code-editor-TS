b5dc475560c8d1292aa5ae84b0c29876c31cec99
Joe <christowel@live.fr>
2022-03-26 10:37:34 -0400

    Adding code editor as a protected route

---
+++ src/pages/code-editor/CodeEditor.tsx
@@ -0,0 +1,46 @@
+// import { styled } from '@mui/material/styles';
+import Layout from 'components/common/layout/Layout';
+
+// Style
+// const CodeEditorDiv = styled('div')(({ theme }) => ({
+//   display: 'flex',
+//   height: '100%',
+//   width: '100%',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   backgroundColor: theme.background,
+// }));
+
+// const FileViewerContainer = styled('div')({
+//   display: 'flex',
+//   flex: 1,
+//   height: '100%',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   maxWidth: '300px',
+//   overflow: 'auto',
+//   borderRight: '1px dashed black',
+// });
+
+// const CodeEditorContainerDiv = styled('div')({
+//   flex: 3,
+//   height: '100%',
+// });
+
+/**
+ *
+ * @returns
+ */
+const CodeEditor = () => {
+  return (
+    <Layout>
+      {/* <CodeEditorDiv>
+        <FileViewerContainer></FileViewerContainer>
+        <CodeEditorContainerDiv></CodeEditorContainerDiv>
+      </CodeEditorDiv> */}
+      Test
+    </Layout>
+  );
+};
+
+export default CodeEditor;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import { useAuth0 } from '@auth0/auth0-react';
+import ProtectedRoute from 'auth/ProtectedRoute';
+import Loading from 'components/common/loading/Loading';
 import paths from './paths';
 
 // SEE REACT OFFICIAL DOCUMENTATION - LAZY and SUSPENCE
 const Home = React.lazy(() => import('pages/home/Home'));
+const CodeEditor = React.lazy(() => import('pages/code-editor/CodeEditor'));
 
 const Routes = () => {
+  //
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  //
   return (
     <Switch>
       <Route exact path={paths.home} component={Home} />
+      <ProtectedRoute exact path={paths.codeEditor} component={CodeEditor} />
     </Switch>
   );
 };
